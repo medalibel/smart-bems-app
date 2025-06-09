174429362cb8df0575e8b894669b792874e9e0d6
Mohamed Ali Belkhir <51958293+medalibel@users.noreply.github.com>
2025-06-09 15:27:12 +0100

    fetch and update user data

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,14 +1,91 @@
 'use client'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import {useRouter} from 'next/navigation'
 import ToggleSwitch from '@/components/toggle-switch';
+
+interface User {
+  username:string | undefined,
+  email:string | undefined,
+  phone_number:string | undefined,
+    address:string | undefined,
+    building_type:string | undefined,
+    city:string | undefined,
+    state:string | undefined,
+    construction_year:string | undefined,
+    first_floor_square_footage:string | undefined,
+    total_square_footage:string | undefined,
+}
 export default function SettingsPage() {
+  const router = useRouter()
+  const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5001/api";
   const [kwh, setKwh] = useState('2000');
   const [threshold, setThreshold] = useState('90');
+  const [updateStatus,setUpdateStatus] = useState('')
+  const [userData,setUserData] = useState<User|null>(null)
+
   const handleSaveBudget = () => {
     // Logic to save the budget would go here
     console.log('Saving budget:', { kwh, threshold });
     alert(`Budget Saved!\nTarget kWh: ${kwh}\nAlert Threshold: ${threshold}%`);
   };
+
+  const updateUserData = async () =>{
+     try {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        router.push('/login');
+        return;
+      }
+      const response = await fetch(`${API_URL}/user/update`,{
+          method:'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          body:JSON.stringify({username:userData?.username, email:userData?.email, phone_number:userData?.phone_number}),
+        });
+      if (!response.ok) {
+        if(response.status === 401)
+        {
+          router.push('/login')
+          return
+        }
+        setUpdateStatus('error')
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      else
+        setUpdateStatus('ok')
+    } catch (error) {
+      console.error("Error fetching weekly data:", error);
+    }
+  }
+  const fetchUserData = async ()=> {
+    try {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        router.push('/login');
+        return;
+      }
+      const response = await fetch(`${API_URL}/user`,{
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+     if (!response.ok) {
+        if(response.status === 401)
+        {
+          router.push('/login')
+          return
+        }
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const result = await response.json();
+      console.log(result)
+      setUserData(result)
+    } catch (error) {
+      console.error("Error fetching weekly data:", error);
+    }
+  }
 
   let devicesData = [
     {
@@ -27,6 +104,11 @@ export default function SettingsPage() {
       status:false,
     },
   ]
+
+  useEffect(()=>{
+    fetchUserData()
+  },[])
+
   return (
     <main className="flex min-h-screen flex-col items-center">
       <section className="w-full max-w-[1200px] p-5 bg-white rounded-xl shadow-md">
@@ -96,7 +178,8 @@ export default function SettingsPage() {
             Full Name
             <input
               type="text"
-              value={'John Doe'}
+              value={userData?.username || ''}
+              onChange={(e)=> setUserData({...userData, username:e.target.value} as User)}
               className="mt-1.5 p-2 border border-gray-300 rounded text-sm text-black"
             />
           </label>
@@ -104,7 +187,8 @@ export default function SettingsPage() {
             Email Address
             <input
               type="email"
-              value={'john.doe@example.com'}
+              value={userData?.email || ''}
+              onChange={(e)=> setUserData({...userData, email:e.target.value} as User)}
               className="mt-1.5 p-2 border border-gray-300 rounded text-sm text-black"
             />
           </label>
@@ -112,7 +196,8 @@ export default function SettingsPage() {
             Phone Number
             <input
               type="tel"
-              value={'+213698877654'}
+              value={userData?.phone_number || ''}
+              onChange={(e)=> setUserData({...userData, phone_number:e.target.value} as User)}
               className="mt-1.5 p-2 border border-gray-300 rounded text-sm text-black"
             />
           </label>
@@ -120,15 +205,45 @@ export default function SettingsPage() {
             Home Address
             <input
               type="text"
-              value={'123 Palm St, Algiers'}
+              value={userData?.address || ''}
+              readOnly
+              className="mt-1.5 p-2 border border-gray-300 rounded text-sm text-black"
+            />
+          </label>
+          <label className="flex-1 basis-[400px] flex flex-col text-sm text-[#0f6f89]">
+            City
+            <input
+              type="text"
+              value={userData?.city || ''}
+              readOnly
+              className="mt-1.5 p-2 border border-gray-300 rounded text-sm text-black"
+            />
+          </label>
+          <label className="flex-1 basis-[400px] flex flex-col text-sm text-[#0f6f89]">
+            State
+            <input
+              type="text"
+              value={userData?.state || ''}
+              readOnly
+              className="mt-1.5 p-2 border border-gray-300 rounded text-sm text-black"
+            />
+          </label>
+          <label className="flex-1 basis-[400px] flex flex-col text-sm text-[#0f6f89]">
+            Building Type
+            <input
+              type="text"
+              value={userData?.building_type || ''}
+              readOnly
               className="mt-1.5 p-2 border border-gray-300 rounded text-sm text-black"
             />
           </label>
           
         </div>
-        <button className="bg-[#0f6f89] text-white border-none px-4 py-[6px] rounded cursor-pointer text-sm">
+        <button className="bg-[#0f6f89] text-white border-none px-4 py-[6px] rounded cursor-pointer text-sm" onClick={()=> updateUserData()}>
             Save Profile
         </button>
+        {updateStatus === 'error' && (<p className=' text-red-400'>{'Could not update user info'}</p>)}
+        {updateStatus === 'ok' && (<p className=' text-green-400'>{'User info updated'}</p>)}
       </section>
     </main>
   );
